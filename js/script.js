0cd929cff25596c1598ad1865a131af9c05970db
shg1998 <moh.nezh1377@gmail.com>
2020-05-04 06:14:55 +0430

    second commit,make the design of page more complete than past,and use ajax!

--- js/script.js
+++ js/script.js
@@ -1,168 +1,53 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-let img = new Image();
-let fileName = "";
-const downloadBtn = document.getElementById("download-btn");
-const UploadFile = document.getElementById("upload-file");
-const revertBtn = document.getElementById("revert-btn");
+$(document).ready(function () {
+    $(".drawDiv").removeAttr("style");
+    $(".dkonvajs-content").removeAttr("style");
+    $(".getImage").click(function (e) {
+//get image base64 & ... :
+// $.ajax({
+//                 url: URL,
+//                 type: "GET",
+//                 dataType: "html",
+//                 async: false,
+//                 crossDomain: "true",
+//                 success: function(data, status) {
+//                     console.log("Status: " + status + "\nData: " + data);
+//                     result = data;
+//
+//                     /* creating image */
+//                     var img = $('<img id="image_id">');
+//                     img.attr("src", "data:image/gif;base64," + data);
+//                     img.appendTo("#canvas");
+//                 }
+//             });
+// get url of image (image address):
 
-revertBtn.addEventListener("click", e => {
-    Caman("#canvas", img, function () {
-        this.revert();
+        // $.ajax({
+        //     url: URL,
+        //     type: "GET",
+        //     dataType: "html",
+        //     async: false,
+        //     crossDomain: "true",
+        //     success: function(data, status) {
+        //         console.log("Status: " + status + "\nData: " + data);
+        //         result = data;
+        //
+        //         dwv.gui.getElement = dwv.gui.base.getElement;
+        //    dwv.gui.displayProgress = function (percent) {};
+        //
+        //    // create the dwv app
+        //    var app = new dwv.App();
+        //    // initialise with the id of the container div
+        //        var options = {
+        //            "containerDivId": "dwv",
+        //            "tools": ["Draw"],
+        //
+        //        };
+        //        app.init(options);
+        //    // load dicom data
+        //
+        //        app.loadURLs(["http://s11.picofile.com/file/8395924226/founder-xray1.png"]);
+        //     }
+        // });
     });
 });
-//add filter and effects:
-document.addEventListener("click", e => {
-    if (e.target.classList.contains("filter-btn")) {
-        if (e.target.classList.contains("brightness-add")) {
-            Caman("#canvas", img, function () {
-                this.brightness(5).render();
-            });
-        } else if (e.target.classList.contains("brightness-remove")) {
-            Caman("#canvas", img, function () {
-                this.brightness(-5).render();
-            });
-        } else if (e.target.classList.contains("contrast-add")) {
-            Caman("#canvas", img, function () {
-                this.contrast(5).render();
-            });
-        } else if (e.target.classList.contains("contrast-remove")) {
-            Caman("#canvas", img, function () {
-                this.contrast(-5).render();
-            });
-        } else if (e.target.classList.contains("saturation-add")) {
-            Caman("#canvas", img, function () {
-                this.saturation(-5).render();
-            });
-        } else if (e.target.classList.contains("saturation-remove")) {
-            Caman("#canvas", img, function () {
-                this.saturation(-5).render();
-            });
-        } else if (e.target.classList.contains("vibrance-add")) {
-            Caman("#canvas", img, function () {
-                this.vibrance(-5).render();
-            });
-        } else if (e.target.classList.contains("vibrance-remove")) {
-            Caman("#canvas", img, function () {
-                this.vibrance(-5).render();
-            });
-        } else if (e.target.classList.contains("vintage-add")) {
-            Caman("#canvas", img, function () {
-                this.vintage().render();
-            });
-        } else if (e.target.classList.contains("lomo-add")) {
-            Caman("#canvas", img, function () {
-                this.lomo().render();
-            });
-        } else if (e.target.classList.contains("clarity-add")) {
-            Caman("#canvas", img, function () {
-                this.clarity().render();
-            });
-        } else if (e.target.classList.contains("sinCity-add")) {
-            Caman("#canvas", img, function () {
-                this.sinCity().render();
-            });
-        } else if (e.target.classList.contains("crossProcess-add")) {
-            Caman("#canvas", img, function () {
-                this.crossProcess().render();
-            });
-        } else if (e.target.classList.contains("pinhole-add")) {
-            Caman("#canvas", img, function () {
-                this.pinhole().render();
-            });
-        } else if (e.target.classList.contains("nostalgia-add")) {
-            Caman("#canvas", img, function () {
-                this.nostalgia().render();
-            });
-        } else if (e.target.classList.contains("herMajesty-add")) {
-            Caman("#canvas", img, function () {
-                this.herMajesty().render();
-            });
-        }
-    }
-});
 
-//uploadFile:
-UploadFile.addEventListener("change", e => {
-    const file = document.getElementById("upload-file").files[0];
-
-    //init  fileReader
-    const reader = new FileReader();
-    if (file) {
-        fileName = file.name;
-        //read the data as url
-        reader.readAsDataURL(file);
-        //write the path of image:
-        document.getElementById("uploaded-name").innerHTML = fileName;
-    }
-
-    //add image to the canvas:
-    reader.addEventListener(
-        "load",
-        () => {
-            //create the img
-            img = new Image();
-
-
-            // $.ajax({
-            //     url: URL,
-            //     type: "GET",
-            //     dataType: "html",
-            //     async: false,
-            //     crossDomain: "true",
-            //     success: function(data, status) {
-            //         console.log("Status: " + status + "\nData: " + data);
-            //         result = data;
-
-            //         /* creating image */
-            //         var img = $('<img id="image_id">');
-            //         img.attr("src", "data:image/gif;base64," + data);
-            //         img.appendTo("#canvas");
-            //     }
-            // });
-
-
-
-            //set src:
-            img.src = reader.result;
-            console.log(img.src);
-            //on image load , add to canvas
-            img.onload = function () {
-                canvas.width = img.width;
-                canvas.height = img.height;
-                context.drawImage(img, 0, 0, img.width, img.height);
-                canvas.removeAttribute("data-caman-id");
-            };
-        },
-        false
-    );
-});
-//download:
-downloadBtn.addEventListener("click", () => {
-    //get the file extension:
-    const fileExt = fileName.slice(-4);
-    //initialize new file name
-    let newFileName;
-
-    //check image type
-    if (fileExt === ".jpg" || fileExt === "png") {
-        newFileName = fileName.substring(0, fileName.length - 4) + "-edited.jpg";
-    }
-
-    //call download
-    download(canvas, newFileName);
-});
-
-function download(canvas, filename) {
-    // init event
-    let ev;
-    //creat Link
-    const link = document.createElement("a");
-    //set Props:
-    link.download = filename;
-    link.href = canvas.toDataURL("image/jpg", 0.8);
-    //new mouse event
-    ev = new MouseEvent("click");
-    //dispatch ev
-    link.dispatchEvent(ev);
-}
